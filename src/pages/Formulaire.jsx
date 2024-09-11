71717511c25dfb6568da799197876f03b336643d
Elimson ADEDODJA <elimsontrade@gmail.com>
2024-09-11 13:35:41 +0200

    Integration ApI version 1

--- src/pages/Formulaire.jsx
+++ src/pages/Formulaire.jsx
@@ -29,57 +29,94 @@ const FormPage = () => {
     setStep(step - 1);
   };
 
+  const onSubmit = async () => {
+    event.preventDefault();
+    try {
+      let res = await fetch("http://vps-b9daf47b.vps.ovh.ca:5000/parcours", {
+        method: "POST",
+        body: JSON.stringify({
+          niveau_etude: "Licence",
+          specialite: "Statistique",
+          matieres: "",
+          passions: "",
+          metier: "Data Scientist",
+        }),
+      });
+      const resJson = await res.json();
+      if (res.status === 200) {
+        console.log(resJson);
+        
+        alert(JSON.stringify(values, null, 2));
+      } else {
+        alert("Erreur");
+      }
+    } catch (error) {
+      alert(error);
+    }
+    
+    /*try {
+        const url: string = `http://localhost:8000/api/posts/${postID}`;
+        await sendRequest(url, 'DELETE');
+        closeModal(event);
+        navigate(`/`);
+        } catch (err: any) {}*/
+ }
+
   // Rendu des étapes
   const renderStep = () => {
     switch (step) {
       case 1:
         return (
           <div>
-            <h2 className="text-2xl font-bold mb-4">Étape 1: Choisir l'Établissement</h2>
+            <h2 className="text-2xl font-bold mb-4">Étape 1: Niveau d'étude</h2>
             <select
               name="etablissement"
               value={formData.etablissement}
               onChange={handleChange}
               className="border p-2 rounded w-full"
             >
-              <option value="">Sélectionner un établissement</option>
-              <option value="etablissement1">Établissement 1</option>
-              <option value="etablissement2">Établissement 2</option>
-              <option value="etablissement3">Établissement 3</option>
+              <option value="">Sélectionner votre Niveau d'étude</option>
+              <option value="etablissement1">BAC</option>
+              <option value="etablissement2">Licence</option>
+              <option value="etablissement3">Master</option>
             </select>
           </div>
         );
       case 2:
         return (
           <div>
-            <h2 className="text-2xl font-bold mb-4">Étape 2: Choisir la Filière</h2>
+            <h2 className="text-2xl font-bold mb-4">Étape 2: Spécialité</h2>
             <select
               name="filiere"
               value={formData.filiere}
               onChange={handleChange}
               className="border p-2 rounded w-full"
             >
-              <option value="">Sélectionner une filière</option>
-              <option value="filiere1">Filière 1</option>
-              <option value="filiere2">Filière 2</option>
-              <option value="filiere3">Filière 3</option>
+              <option value="">Sélectionner une Spécialité</option>
+              <option value="filiere1">Agronome</option>
+              <option value="filiere2">Statistique</option>
+              <option value="filiere3">Electricien</option>
             </select>
           </div>
         );
       case 3:
         return (
           <div>
-            <h2 className="text-2xl font-bold mb-4">Étape 3: Autre Information</h2>
+            <h2 className="text-2xl font-bold mb-4">Étape 3: Métier</h2>
             <select
               name="autre"
               value={formData.autre}
               onChange={handleChange}
               className="border p-2 rounded w-full"
             >
-              <option value="">Sélectionner une option</option>
-              <option value="option1">Option 1</option>
-              <option value="option2">Option 2</option>
-              <option value="option3">Option 3</option>
+              <option value="">Sélectionner un Métier</option>
+              <option value="option3">Data Scientiste</option>
+              <option value="option1">Statistique</option>
+              <option value="option2">Informatique</option>
+              <option value="option3">Agriculture</option>
+              <option value="option3">Data Analyst</option>
+              <option value="option3">Developpeur Web</option>
+              
             </select>
           </div>
         );
@@ -117,12 +154,12 @@ const FormPage = () => {
 
           {/* Bouton Soumettre */}
           {step === 3 && (
-            <Link
-              to="/SearchResults"
+            <button
+              onClick={onSubmit}
               className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 ml-auto"
             >
               Soumettre
-            </Link>
+            </button>
           )}
         </div>
       </div>
